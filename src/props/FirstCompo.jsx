fb37fa763cb476f841f5a01d573d8471c11e3fac
Burhan149 <burhanyasirk@gmail.com>
2023-12-20 02:08:24 +0500

    A B Function with onClick or onchange

--- src/props/FirstCompo.jsx
+++ src/props/FirstCompo.jsx
@@ -13,4 +13,3 @@ export const FirstCompo =()=> {
 export default FirstCompo;
 
 
-
--- src/state/A.jsx
+++ src/state/A.jsx
@@ -1,25 +1,30 @@
 
-import React, { useState } from 'react'
+
+import React from 'react'
 
 const A = () => {
-  const [name , updateName] = React.useState('Hadi Raza');
-    const [lastName , updateLastName] = React.useState('Rizvi Hadi Raza')
-     function myClicks(){
-        alert('Change the name')
-        console.log("Hi")
-     }
-    return (
+// State component
+  const [name , updateName] = React.useState("Hadi RAza")
+  const [lastName , updateLastName] = React.useState("Yasir Tizvi")
+
+
+  function myClicks(){
+    alert("hello change the name for me")
+    console.log("hi jan")
+    updateName ("abbas ali shah")
+
+  }
+  return (
     <div>
       <h1>A - {name}</h1>
-      <button onClick={ () => {
-        alert('CHange the name for me ')
-        // updateName("Ali abbas shah")
-      }}>Press to change the name </button>
-      
-      {/* <input type="text" value={lastName} onChange={(e) => console.log(e.target.name)} name="lastname"/> */}
+      {/* <button 
+      onClick= {(myClicks)}>Press to change</button> */}
+
+      <input type="text"  value={lastName}  onChange={(e) => console.log(e.target.name)}/>
     </div>
   )
 }
 
-export default A;
+export default A
+
 
